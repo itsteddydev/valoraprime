15fbabb57d0ee8997dcc5c068da7c85f471dbb8b
justcoddev <edisonpaulcrz@gmail.com>
2025-01-02 11:27:58 -0500

    Agregar configuración de i18next para namespaces y ruta de carga de archivos de traducción

--- src/config/i18next.config.js
+++ src/config/i18next.config.js
@@ -5,12 +5,27 @@ import { initReactI18next } from 'react-i18next';
 i18n.use(Backend)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
-    lng: localStorage.getItem('language') || 'en', // Usa el idioma de localStorage o el de fallback
+    fallbackLng: 'en', // Idioma predeterminado
+    lng: localStorage.getItem('language') || 'en', // Obtiene el idioma de localStorage o usa el predeterminado
     debug: false,
     interpolation: {
       escapeValue: false,
     },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json', // Configura la ruta según tu estructura
+    },
+    ns: [
+      'calculatorForm',
+      'faqSection',
+      'featuresSection',
+      'footer',
+      'header',
+      'heroSection',
+      'notFound',
+      'resultsView',
+      'testimonialsSection',
+    ], // Nombres de los namespaces
+    defaultNS: 'header', // Namespace predeterminado (opcional)
   });
 
 export default i18n;
