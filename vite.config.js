4bda6a8f11c8dd78765cfa3817023216461c6947
justcoddev <edisonpaulcrz@gmail.com>
2025-01-02 15:14:58 -0500

    Desactivar la opción de habilitar el modo de desarrollo en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     devOptions: {
-      enabled: true
+      enabled: false
     },
     injectRegister: 'auto',
     includeAssets: ['public/windows11', 'public/android', 'public/ios'],
